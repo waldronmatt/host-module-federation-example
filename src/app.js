96f35389308cbff8cc13bd659d2b6af1c41ae8e5
waldronmatt <mattwaldron@ymail.com>
2021-01-28 23:16:32 -0500

    revert change, add comments

--- src/app.js
+++ src/app.js
@@ -8,9 +8,15 @@ heading.render("HOST (Module Federation)");
 const image = new moduleFederation();
 image.render();
 
-jQueryTest();
-
 import("FormApp/initContactForm").then((initContactFormModule) => {
   const initContactForm = initContactFormModule.default;
   initContactForm();
 });
+
+/*
+  This import should be at the bottom
+  We're intentially throwing an error to test the jQuery version
+  The error will prevent other content from being rendered to the DOM
+  if it is below this function call
+*/
+jQueryTest();
